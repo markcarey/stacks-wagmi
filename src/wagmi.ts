f88da5042f17ae346b5c3a04176d12c546f13d30
markcarey <mark@markcarey.com>
2023-06-25 04:31:30 -0400

    abis

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,19 +1,18 @@
 import { configureChains, createConfig } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
+import { goerli, polygon } from 'wagmi/chains'
 import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
 
 import { publicProvider } from 'wagmi/providers/public'
 
 const walletConnectProjectId = '5689d3b8c663b1f930d375bed1f9a8ba'
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
-  [
-    publicProvider(),
-  ],
+  [goerli, polygon],
+  [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY ? process.env.NEXT_PUBLIC_ALCHEMY_API_KEY: '' })],
 )
 
 export const config = createConfig({
